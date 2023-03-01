ed33599eb4722aeacb7c72bd75b49bdbb70d8a6c
Adotel <adriandotel@gmail.com>
2023-03-01 20:13:45 +0100

    query, and id for url

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,26 @@
 
-const { response } = require('express');
+const { response, request } = require('express');
 
-const getUsers = (req, res = response) => {
-    res.json({ msg: 'Get' });
+const getUsers = (req = request, res = response) => {
+
+    // Comes after de ? in the url
+    const { nom, num = 'No Num' } = req.query;
+
+    res.json({ msg: 'Get', nom, num });
 }
 
 const createUsers = (req, res = response) => {
-    res.json({ msg: 'Create' });
+
+    const body = req.body;
+
+    res.json({ msg: 'Create', body });
 }
 
 const editUsers = (req, res = response) => {
+
+    // Comes from routers
+    const id = req.params.id;
+
     res.json({ msg: 'Edit' });
 }
 
--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,7 @@ const router = Router();
 
 router.get('/', getUsers);
 
-router.put('/', editUsers);
+router.put('/:id', editUsers);
 
 router.post('/', createUsers);
 
