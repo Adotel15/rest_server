eee0d47dba5a059fb829f53501669ef330a3f3d1
Adotel <adriandotel@gmail.com>
2023-03-10 20:07:10 +0100

    added middleware

--- middleware/validation.js
+++ middleware/validation.js
@@ -7,10 +7,8 @@ const inputValidations = (req = request, res = response, next) => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()) return res.status(400).json(errors);
-    
 
     next();
-
 }
 
 module.exports = {
--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,8 @@ const {
     editUsers,
     patchUser,
     deleteUsers
-} = require('../controllers/user')
+} = require('../controllers/user');
+const { inputValidations } = require('../middleware/validation');
 
 const router = Router();
 
@@ -22,7 +23,8 @@ router.post(
         check('mail', 'Mail not valid').isEmail(),
         check('name', 'Name required').not().isEmpty(),
         check('password', 'Passowrd atleast 6 characters').isLength({ min: 6 }),
-        check('role', 'Not valid role').isIn(['ADMIN_ROLE', 'USER_ROLE'])
+        check('role', 'Not valid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        inputValidations
     ], 
     createUsers
 );
